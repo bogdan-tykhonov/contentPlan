202ae556de514c009579c5adfe210e48421bf383
cryptoman423 <bogdan.v.t@ukr.net>
2020-07-18 17:11:14 +0300

    c
    c

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ $(document).ready(function(){
     $('.slider').slick({
     });
 })
+let windowWidth = $(window).width();
+if(windowWidth <= 768){
+    $('.text-content').removeClass('after-line');
+    $('.img-wrapper').addClass('after-line');
+}
